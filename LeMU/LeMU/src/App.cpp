eaa10bb9a8db815aa123bc99e8130d028cfc5309
cjmms <brandon.xyw@gmail.com>
2021-05-07 19:56:41 -0700

    Pipeline Layout

--- LeMU/LeMU/src/App.cpp
+++ LeMU/LeMU/src/App.cpp
@@ -3,6 +3,22 @@
 
 namespace LeMU
 {
+	FirstApp::FirstApp()
+	{
+		CreatePipelinelayout();
+		CreatePipeline();
+		CreateCommandBuffers();
+	}
+
+
+	FirstApp::~FirstApp()
+	{
+		vkDestroyPipelineLayout(device.device(), pipelineLayout, nullptr);
+	}
+
+
+
+
 	void FirstApp::run()
 	{
 		while (!window.ShouldClose())
@@ -11,6 +27,50 @@ namespace LeMU
 		}
 	}
 
+
+
+
+	void FirstApp::CreatePipelinelayout()
+	{
+		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+		pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+		pipelineLayoutInfo.setLayoutCount = 0;
+		pipelineLayoutInfo.pSetLayouts = nullptr;
+		pipelineLayoutInfo.pushConstantRangeCount = 0;
+		pipelineLayoutInfo.pPushConstantRanges = nullptr;
+
+		if (vkCreatePipelineLayout(device.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) !=
+			VK_SUCCESS)
+		{
+			throw std::runtime_error("Failed to create pipeline layout.");
+		}
+
+	}
+
+
+
+	void FirstApp::CreatePipeline()
+	{
+		auto pipelineConfig = Pipeline::DefaultPipelineConfigInfo(swapChain.width(), swapChain.height());
+		pipelineConfig.renderPass = swapChain.getRenderPass();
+		pipelineConfig.pipelineLayout = pipelineLayout;
+		pipeline = std::make_unique<Pipeline>(
+			device,
+			"shaders/simple_shader.vert.spv",
+			"shaders/simple_shader.frag.spv",
+			pipelineConfig);
+	}
+
+
+	void FirstApp::CreateCommandBuffers()
+	{
+
+	}
+
+	void FirstApp::DrawFrame()
+	{
+
+	}
 
 
 }
--- LeMU/LeMU/src/App.hpp
+++ LeMU/LeMU/src/App.hpp
@@ -5,6 +5,8 @@
 #include "Device.hpp"
 #include "SwapChain.hpp"
 
+#include <memory>
+
 namespace LeMU
 {
 	class FirstApp
@@ -13,17 +15,26 @@ namespace LeMU
 		static constexpr int WIDTH = 800;
 		static constexpr int HEIGHT = 600;
 
+		FirstApp();
+		~FirstApp();
+		
+		FirstApp(const FirstApp&) = delete;
+		FirstApp& operator=(const FirstApp&) = delete;
 
 		void run();
+
 	private:
+		void CreatePipelinelayout();
+		void CreatePipeline();
+		void CreateCommandBuffers();
+		void DrawFrame();
+
 		Window window{ WIDTH, HEIGHT, "Hello Vulkan" };
 		Device device{window};
 		SwapChain swapChain{ device, window.getExtent() };
-		Pipeline pipeline{
-			device,
-			"shaders/simple_shader.vert.spv",
-			"shaders/simple_shader.frag.spv",
-			Pipeline::DefaultPipelineConfigInfo(WIDTH, HEIGHT) };
+		std::unique_ptr<Pipeline> pipeline;
+		VkPipelineLayout pipelineLayout;
+		std::vector<VkCommandBuffer> commandBuffers;
 	};
 
 
